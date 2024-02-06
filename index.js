0bc5548722b1e6346d32447f945dcd3206cae031
spcsgaba <spcsgaba@gmail.com>
2024-02-06 11:55:42 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+const port = process.argv[3] || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/api', (req, res) => {
+  res.json({"msg": "Hello world"});
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+})
